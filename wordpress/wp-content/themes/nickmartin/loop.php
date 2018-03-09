529d4f3a2b290397e3181860e204b76b128ba9b1
nmartinMCAD <nmartin@mcad.edu>
2018-03-09 13:38:12 -0700

    Week 8 Final

---
+++ wordpress/wp-content/themes/nickmartin/loop.php
@@ -0,0 +1,12 @@
+<!--<?php echo basename(__FILE__); ?>-->
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <a href="<?php the_permalink(); ?>">
+      <article class="post">
+        <?php the_title( '<h2>', '</h2>' );?>
+        <?php the_content();?>
+      </article>
+    </a>
+  <?php endwhile; else : ?>
+    <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+  <?php endif; ?>
+<!--<?php echo "END OF " . basename(__FILE__); ?>-->
--- wordpress/wp-content/themes/nickmartin/main.php
+++ wordpress/wp-content/themes/nickmartin/main.php
@@ -1,12 +1,5 @@
 <!--<?php echo basename(__FILE__); ?>-->
 <main>
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <article class="post">
-      <?php the_title( '<h2>', '</h2>' );?>
-      <?php the_content();?>
-    </article>
-  <?php endwhile; else : ?>
-    <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-  <?php endif; ?>
+  <?php get_template_part('loop'); ?>
 </main>
 <!--<?php echo "END OF " . basename(__FILE__); ?>-->
